432834d6c98f7863b8833dff4a987d2d1a4a95ce
Roman <gremkil@gmail.com>
2016-06-09 11:40:00 +0300

    *общий класс для взаимодействия с сетью

---
+++ SuperCore/SuperCore/SuperNet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperCore
+{
+    public abstract class SuperNet : Super
+    {
+        private readonly ConcurrentDictionary<Guid, TaskCompletionSource<CallResult>> mWaitingCalls 
+            = new ConcurrentDictionary<Guid, TaskCompletionSource<CallResult>>(); 
+
+        public override CallResult SendCall(CallInfo info)
+        {
+            var tcs = new TaskCompletionSource<CallResult>();
+            mWaitingCalls.TryAdd(info.CallID, tcs);
+            SendData(info);
+            return tcs.Task.Result;
+        }
+
+        protected abstract void SendData(CallInfo info);
+
+        protected abstract void SendData(CallResult result);
+
+        protected void ReciveData(CallResult result)
+        {
+            TaskCompletionSource<CallResult> tcs;
+            mWaitingCalls[result.CallID].SetResult(result);
+            mWaitingCalls.TryRemove(result.CallID, out tcs);
+        }
+
+        protected void ReciveData(CallInfo info)
+        {
+            var result = Call(info);
+            SendData(result);
+        }
+    }
+}
