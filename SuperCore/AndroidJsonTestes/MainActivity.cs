60002faad6e9a762e120b29abc4f36d712e86452
Roman <Roman@DESKTOP-19EMMBL>
2017-04-03 20:36:35 +0300

    *test projects

---
+++ SuperCore/AndroidJsonTestes/MainActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Android.App;
+using Android.Widget;
+using Android.OS;
+using SuperJson;
+using SuperJson.Parser;
+
+namespace AndroidJsonTestes
+{
+    public class TestClass
+    {
+        private static readonly Lazy<TestClass> mInstance = new Lazy<TestClass>(() => new TestClass());
+        public static TestClass Instance => mInstance.Value;
+
+        [DoNotSerialise]
+        const string FileName = "Data.json";
+        [DoNotSerialise]
+        private List<TestClass> mProjectsCashe;
+        [DoNotSerialise]
+        private readonly ConcurrentDictionary<int, List<TestClass>> mVolumeCashe = new ConcurrentDictionary<int, List<TestClass>>();
+        [DoNotSerialise]
+        private readonly ConcurrentDictionary<int, List<TestClass>> mChaptersCache = new ConcurrentDictionary<int, List<TestClass>>();
+        [DoNotSerialise]
+        private readonly ConcurrentDictionary<int, TestClass> mTextCace = new ConcurrentDictionary<int, TestClass>();
+        [DoNotSerialise]
+        private int mLastChapterId;
+        [DoNotSerialise]
+
+        public bool Loaded { get; private set; }
+        public List<string> ReadChapterUrls { get; private set; } = new List<string>();
+        public Dictionary<string, int> ChapterSctolls { get; private set; } = new Dictionary<string, int>();
+        //[DoNotSerialise]
+        public Dictionary<int, int> ProjectOrders { get; private set; } = new Dictionary<int, int>();
+    }
+
+    [Activity(Label = "AndroidJsonTestes", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView (Resource.Layout.Main);
+
+            var obj = new TestClass();
+            for (int i = 0; i < 150; i++)
+            {
+                obj.ProjectOrders[i] = 0;
+                obj.ChapterSctolls[i.ToString()] = i % 4;
+                obj.ReadChapterUrls.Add(i.ToString());
+            }
+
+            var ser = new SuperJsonSerializer();
+            var data = ser.Serialize(obj);
+        }
+    }
+}
+
--- SuperCore/JsonTestes/Program.cs
+++ SuperCore/JsonTestes/Program.cs
@@ -1,71 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using SuperJson;
+using SuperJson.Objects;
 using SuperJson.Parser;
 
 namespace JsonTestes
 {
     class Program
-	{
-        private class Test1
-        {
-            private static readonly Lazy<Test1> mInstance = new Lazy<Test1>(() => new Test1());
-            public static Test1 Instance => mInstance.Value;
-
-            public int Field1;
-            public int Property1 { get; set; }
-
-            [DoNotSerialise]
-            public int Property2 { get; set; }
-
-            public Dictionary<int, int> DictProperty = new Dictionary<int, int>(); 
-        }
+    {
 
         static void Main(string[] args)
         {
-//            var result = "";
-//            var className = "DelegateFuncWrapper";
-//            for (int i = 1; i < 10; i++)
-//            {
-//                var genericTypesSplitted = Enumerable.Range(1, i).Select(n => $"T{n}").ToList();
-//                var genericTypes = genericTypesSplitted.Aggregate((a, b) => $"{a}, {b}");
-//                var argsDeclaration = genericTypesSplitted.Select(t => $"{t} arg{t.Substring(1)}").Aggregate((a, b) => $"{a}, {b}");
-//                var argsNames = Enumerable.Range(1, i).Select(n => $"arg{n}").Aggregate((a, b) => $"{a}, {b}");
-//                var decl = $@"
-//public class {className}<{genericTypes}, TResult>
-//{{
-//	private readonly Delegate mSubject;
-
-//	public {className}(Delegate subject)
-//	{{
-//        mSubject = subject;
-//	}}
-
-//    public TResult Invoke({argsDeclaration})
-//    {{
-//        return (TResult)mSubject.DynamicInvoke({argsNames});
-//    }}
-//}}
-//";
-//                result += $"{decl}{Environment.NewLine}{Environment.NewLine}";
-//            }
-
             var parser = new SuperJsonParser();
-            var serializer = new SuperJsonSerializer();
-            var json = serializer.Serialize(new[] { 1, 2.7, 3, 5 });
-            var list = new List<int>() { 0, 5, 8, 4 };
-            var json1 = serializer.Serialize(list);
-            var cl = new Test1() { Field1 = 5, Property1 = 7, Property2 = 9 };
-            var json2 = serializer.Serialize(cl);
 
-            var parsed = parser.Parse(json1);
+            var dir = args[0];
 
-            var unparsed = parser.Write(parsed);
+            parser.Parse("-0.1");
 
-            var arr1 = serializer.Deserialize(json);
-            var list1 = serializer.Deserialize(json1);
-            var cl1 = serializer.Deserialize(json2);
+            var files = Directory.EnumerateFiles(dir, "*.json");
+
+            foreach (var file in files)
+            {
+                var fname = Path.GetFileName(file) ?? "";
+                if (fname.StartsWith("y_"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else if (fname.StartsWith("n_"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                Console.WriteLine($"Parsing: {file}");
+                SuperToken result = null;
+                try
+                {
+                    result = parser.Parse(File.ReadAllText(file));
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                }
+                Console.ForegroundColor = result == null ? ConsoleColor.Red : ConsoleColor.Green;
+                Console.WriteLine($"Parsing result: {(result != null ? "Success" : "Failed")}");
+            }
 
             Console.ReadLine();
         }
